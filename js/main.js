69ffd2f619eb74ebf84272bfecf89f2dafdc79fa
zeljko lalic <zeljkolalic@zeljkos-MacBook-Pro.local>
2019-12-25 08:37:57 +0100

    Updated form

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 // Initialize and add the map
 function initMap() {
   // Your location
-  const loc = { lat: 44.852150, lng: 20.345020 };
+  const loc = { lat: 44.837673, lng: 20.416835 };
   // Centered map on location
-  const map = new google.maps.Map(document.querySelector('.map') 
-   , {
-       zoom: 14,
-       center: loc
-   });
-   // The marker, positioned at location
-   const marker = new google.maps.Marker({ position: loc, map: map });
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
-
 // Sticky menu background
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function() {
   if (this.window.scrollY > 150) {
-    this.document.querySelector('#navbar').style.opacity = 0.8;
+    this.document.querySelector("#navbar").style.opacity = 0.8;
   } else {
-    document.querySelector('#navbar').style.opacity = 1;
+    document.querySelector("#navbar").style.opacity = 1;
   }
 });
 
-
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function(event) {
-  if (this.hash !== '') {
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
+    $("html, body").animate(
       {
         scrollTop: $(hash).offset().top - 100
       },
@@ -39,4 +36,4 @@ $('#navbar a, .btn').on('click', function(event) {
   }
 });
 
-sr.reveal('.btn1', { origin: 'top', distance: '70px', duration: 2000});
+sr.reveal(".btn1", { origin: "top", distance: "70px", duration: 2000 });
