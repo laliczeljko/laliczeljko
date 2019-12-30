5ee7eb3ea374c10161b971bd66cf120ace4ed5d7
zeljko lalic <zeljkolalic@zeljkos-MacBook-Pro.local>
2019-12-30 08:26:53 +0100

    Updated form

--- js/main.js
+++ js/main.js
@@ -20,6 +20,33 @@ window.addEventListener("scroll", function() {
   }
 });
 
+// Mouse Circle Animation
+document.onmousemove = animateCircles;
+
+var colors = ["#ffd500", "#f0d000", "#dbbe00", "#c2a802", "#a38e02"];
+
+function animateCircles(event) {
+  var circle = document.createElement("div");
+  circle.setAttribute("class", "circle");
+  document.body.appendChild(circle);
+
+  circle.style.left = event.clientX + "px";
+  circle.style.top = event.clientY + "px";
+
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  circle.style.borderColor = color;
+
+  circle.style.transition = "all 0.3s linear 0s";
+
+  circle.style.left = circle.offsetLeft - 20 + "px";
+  circle.style.top = circle.offsetTop - 20 + "px";
+
+  circle.style.width = "50px";
+  circle.style.height = "50px";
+  circle.style.borderWidth = "5px";
+  circle.style.opacity = 0;
+}
+
 // Smooth Scrolling
 $("#navbar a, .btn").on("click", function(event) {
   if (this.hash !== "") {
